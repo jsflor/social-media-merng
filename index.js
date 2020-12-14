8cdddefcb22648fdf76a34f854d593863bdcf940
Sebastian Flor <sflor@telecomboutique.com>
2020-12-14 12:26:42 +0100

    feat(server): register and login users

--- index.js
+++ index.js
@@ -1,44 +1,18 @@
 const {ApolloServer} = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 /* CONFIG */
-const {MONGODB} = require('./config');
-/* MODELS */
-const Post = require('./models/Post');
-const User = require('./models/User');
-
-const typeDefs = gql`
-    type Post {
-        id: ID!,
-        body: String!,
-        createdAt: String!,
-        username: String!
-    }
-    type Query {
-        getPosts: [Post]
-    }
-`;
-
-const resolvers = {
-    Query: {
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            } catch (e) {
-                throw new Error(e);
-            }
-        }
-    }
-};
+const {MONGO_DB} = require('./config');
+/* GRAPHQL */
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
    typeDefs,
    resolvers
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(MONGO_DB, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('MongoDB connected!');
         return server.listen({port: 8080});
