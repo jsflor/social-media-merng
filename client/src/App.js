f814bda6b3d475f5bfc12a88e28680fd5127621d
Sebastian Flor <sflor@telecomboutique.com>
2020-12-14 16:39:03 +0100

    feat(client): semantic ui and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Container} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+import {HomePage, LoginPage, RegisterPage} from './pages';
+import {NavBar} from './components';
+
 const App = () => {
     return (
-        <div>
-            <h1>Hi world!</h1>
-        </div>
+        <Router>
+            <Container>
+                <NavBar/>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/login' component={LoginPage}/>
+                <Route exact path='/register' component={RegisterPage}/>
+            </Container>
+        </Router>
     );
 }
 export default App;
